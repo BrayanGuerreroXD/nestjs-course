25b86d30b684cb3ca98d30e4525c7fd05afcfd00
Brayan Guerrero <brayanstewartguor@gmail.com>
2024-06-24 11:27:11 -0500

    Configuración y variables de entorno | Clase 1 | NestJS de 0 a 100

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './constants/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  app.enableCors(CORS);
+
+  app.setGlobalPrefix('api');
+  
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
